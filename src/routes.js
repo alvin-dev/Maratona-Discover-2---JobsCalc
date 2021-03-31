a02ae1b35b52c3c399b985b19353f6ef25ca0b4d
Alvaro <alvinweeb@gmail.com>
2021-03-30 21:44:44 -0300

    jobs

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,53 @@
-const express = require('express');
-const routes = express.Router()
+const express = require('express'); // lib para criar o servidor 
+const routes = express.Router() // funcionalidade do express para criar as rotas
 
-const basePath = __dirname + "/views"
-// request, response
-routes.get('/', (request, response) => response.sendFile(basePath + "/index.html"))
-routes.get('/job', (request, response) => response.sendFile(basePath + "/job.html"))
-routes.get('/job/edit', (request, response) => response.sendFile(basePath + "/job-edit.html"))
-routes.get('/profile', (request, response) => response.sendFile(basePath + "/profile.html"))
+const profile = {
+    name: "Alvaro",
+    avatar: "https://github.com/alvin-dev.png",
+    "monthly-budget": 3000,
+    "days-per-week": 5,
+    "hours-per-day": 5,
+    "vacation-per-year": 4
+}
 
+const views = __dirname + "/views/"
+// __dirname é sempre o diretório em que o script atualmente em execução reside. Então, se você digitou __dirname em /d1/d2/myscript.js, o valor seria /d1/d2.
+
+const jobs = [
+    {
+        id: 1,
+        name: "Pizzaria Guloso",
+        "daily-hours": 2,
+        "total-hours": 60,
+        created_at: Date.now()
+    },
+    {
+        id: 2,
+        name: "OneTwo Project",
+        "daily-hours": 3,
+        "total-hours": 47,
+        created_at: Date.now()
+    }
+]
+
+routes.get('/', (req, res) => res.render(views + "index", { jobs }))
+routes.get('/job', (req, res) => res.render(views + "job"))
+routes.post('/job', (req, res) => {
+    // req.body = { name: 'land page', 'daily-hours': '4', 'total-hours': 
+
+    const jobId = jobs[jobs.length - 1]?.id || 1;
+
+    jobs.push({
+        id: lastId + 1,
+        name: req.body.name,
+        "daily-hours": req.body["daily-hours"],
+        "total-hours": req.body["total-hours"],
+        created_at: Date.now() // atribuindo data de hoje
+    })
+    return res.redirect('/')
+})
+routes.get('/job/edit', (req, res) => res.render(views + "job-edit"))
+routes.get('/profile', (req, res) => res.render(views + "profile", { profile }))
 
 
 module.exports = routes
--- src/server.js
+++ src/server.js
@@ -2,8 +2,14 @@ const express = require("express")
 const server = express()
 const routes = require('./routes.js')
 
+// usando template engine
+server.set('view engine', 'ejs')
+
 //habilitar arquivos estaticos
 server.use(express.static('public'))
+
+// usar o req.body
+server.use(express.urlencoded({ extended: true }))
 
 // routes
 server.use(routes)
