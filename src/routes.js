09c6c2e37ed8f508fe59990ca42c75a73a8da305
Alvaro <alvinweeb@gmail.com>
2021-04-01 15:40:07 -0300

    formatando job, adicionando ao html

--- src/routes.js
+++ src/routes.js
@@ -7,7 +7,8 @@ const profile = {
     "monthly-budget": 3000,
     "days-per-week": 5,
     "hours-per-day": 5,
-    "vacation-per-year": 4
+    "vacation-per-year": 4,
+    "value-hour": 75
 }
 
 const views = __dirname + "/views/"
@@ -18,7 +19,7 @@ const jobs = [
         id: 1,
         name: "Pizzaria Guloso",
         "daily-hours": 2,
-        "total-hours": 60,
+        "total-hours": 1,
         created_at: Date.now()
     },
     {
@@ -30,7 +31,63 @@ const jobs = [
     }
 ]
 
-routes.get('/', (req, res) => res.render(views + "index", { jobs }))
+
+
+
+function remainingDays(job) {
+    //calculo de tempo restante
+    const remainingDays = (job["total-hours"] / job["daily-hours"]).toFixed()
+
+    const createdDate = new Date(job.created_at)
+    const dueDay = createdDate.getDate() + Number(remainingDays)
+    const dueDateInMs = createdDate.setDate(dueDay)
+
+    const timeDiffInMs = dueDateInMs - Date.now()
+    
+    //trandformar milli em dias
+    const dayInMs = 1000 * 60 * 60 * 24
+    const dayDiff = Math.floor(timeDiffInMs / dayInMs)
+    
+    //restam x dias
+    return dayDiff 
+}
+
+
+routes.get('/', (req, res) => {
+
+    // mapeia os objetos do jobs e nomeia como job
+    const updatedJobs = jobs.map((job) => {
+
+        // ajustes no job
+        const remaining = remainingDays(job)
+        const status = remaining <= 0 ? 'done' : 'progress'
+
+        return {
+            ...job,
+            remaining,
+            status,
+            budget: profile["value-hour"] * job["total-hours"]
+        }
+    })
+
+    
+
+    return res.render(views + "index", { jobs: updatedJobs })
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 routes.get('/job', (req, res) => res.render(views + "job"))
 routes.post('/job', (req, res) => {
     // req.body = { name: 'land page', 'daily-hours': '4', 'total-hours': 
