d449a8bf7846ce6fcf3fd51c4c75a193064ccae4
Alvaro <alvinweeb@gmail.com>
2021-04-27 18:47:29 -0300

    delete, update

--- src/controllers/JobController.js
+++ src/controllers/JobController.js
@@ -9,11 +9,7 @@ module.exports = {
     },
 
     async save(req, res) {
-        const jobs = await Job.get();
-        const lastId = jobs[jobs.length - 1]?.id || 0;
-
         await Job.create({
-            id: lastId + 1,
             name: req.body.name,
             "daily-hours": req.body["daily-hours"],
             "total-hours": req.body["total-hours"],
@@ -42,38 +38,22 @@ module.exports = {
     
     async update(req, res) {
         const jobId = req.params.id
-        const jobs = await Job.get()
-
-        const job = jobs.find(job => Number(job.id) === Number(jobId))
-
-        if (!job) {
-            return res.send("Job not found !!")
-        }
-
+        
         const updatedJob = {
-            ...job,
             name: req.body.name,
             "total-hours": req.body["total-hours"],
-            "daily-hours": req.body["daily-hours"],
+            "daily-hours": req.body["daily-hours"]
         }
 
-        const newJobs = jobs.map(job => {
-
-            if (Number(job.id) === Number(jobId)) {
-                job = updatedJob
-            }
-            return job
-        })
-
-        Job.update(newJobs)
+        await Job.update(updatedJob, jobId)
 
         res.redirect('/job/' + jobId)
     },
 
-    delete(req, res) {
+    async delete(req, res) {
         const jobId = req.params.id
 
-        Job.delete(jobId)
+        await Job.delete(jobId)
 
         return res.redirect('/')
     }
--- src/model/Job.js
+++ src/model/Job.js
@@ -19,12 +19,25 @@ module.exports = {
         }))
     },
 
-    update(newJob) {
-        data = newJob
+    async update(updatedJob, jobId) {
+        const db = await Database()
+
+        await db.run(`UPDATE jobs SET
+            name = "${updatedJob.name}",
+            daily_hours = ${updatedJob["daily-hours"]},
+            total_hours = ${updatedJob["total-hours"]}
+            WHERE id = ${jobId}
+        `)
+
+        await db.close()
     },
 
-    delete(id) {
-        data = data.filter(job => Number(job.id) !== Number(id))
+    async delete(id) {
+        const db = await Database()
+
+        await db.run(`DELETE FROM jobs WHERE id = ${id}`)
+
+        await db.close()
     },
     
     async create(newJob) {
@@ -38,8 +51,8 @@ module.exports = {
             created_at
         ) VALUES (
             "${newJob.name}", 
-            ${newJob.daily_hours}
-            ${newJob.total_hours}
+            ${newJob["daily-hours"]},
+            ${newJob["total-hours"]},
             ${newJob.created_at}
         )`)
 
